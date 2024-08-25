3a93d25a1efc35d85657c4dffdaeb4f5182f26d2
Fredy <116135955+F4k3r22@users.noreply.github.com>
2024-08-25 14:07:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nanograd",
+    name="NanoGrad",
     version="0.1.0",
     author="Rivera.ai/Fredy",
     author_email="riveraaai200678@gmail.com",
